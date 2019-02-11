96303f99bc327a50e80cdfe8b81ca46f86f3e5c2
masay <masaya0322713181@ezweb.ne.jp>
2019-02-11 16:37:41 +0900

    Updates

---
+++ precache-manifest.82043e20477ec92896b0d6d6aaefc8f5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc40c7b414bcbf055bb3",
+    "url": "/cookpad_react/static/js/runtime~main.fc40c7b4.js"
+  },
+  {
+    "revision": "6c9064a12f1647576c25",
+    "url": "/cookpad_react/static/js/main.6c9064a1.chunk.js"
+  },
+  {
+    "revision": "887281b50e487a87ec26",
+    "url": "/cookpad_react/static/js/1.887281b5.chunk.js"
+  },
+  {
+    "revision": "5631563095292de313b913b379534df3",
+    "url": "/cookpad_react/index.html"
+  }
+];
